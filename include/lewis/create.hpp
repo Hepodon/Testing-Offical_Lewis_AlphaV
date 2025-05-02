678fb942a63d300247eb2a4a8c7d84d0cff7bdab
Hepodon <brandonlewis0120@gmail.com>
2025-05-01 23:08:01 -0500

    Saved shape names and points

--- include/lewis/create.hpp
+++ include/lewis/create.hpp
@@ -1,9 +1,29 @@
 #pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Box {
+  string name;
+  int x, y, width, height;
+};
+
+struct Triangle {
+  string name;
+  int x1, y1, x2, y2, x3, y3;
+};
 
 class Creator {
 public:
-  void box(int x, int y, int width, int height);
-  void triangle(int x1, int y1, int x2, int y2, int x3, int y3);
+  void box(string name, int x, int y, int width, int height);
+  void triangle(string name, int x1, int y1, int x2, int y2, int x3, int y3);
+
+  vector<Box> boxes;
+  vector<Triangle> triangles;
+
+  vector<string> shape_Box_Names;
+  vector<string> shape_Triangle_Names;
 };
 
 extern Creator create;
--- src/create.cpp
+++ src/create.cpp
@@ -2,14 +2,36 @@
 #include "liblvgl/core/lv_obj.h"
 #include "liblvgl/lvgl.h"
 #include "liblvgl/widgets/line/lv_line.h"
+#include <algorithm>
+#include <string>
+#include <vector>
 
-void Creator::box(int x, int y, int width, int height) {
+void Creator::box(string box_Name, int x, int y, int width, int height) {
+  // Check for duplicate box name
+  auto exists =
+      std::find(shape_Box_Names.begin(), shape_Box_Names.end(), box_Name);
+  if (exists != shape_Box_Names.end())
+    return;
+
+  shape_Box_Names.push_back(box_Name);
+  boxes.push_back({box_Name, x, y, width, height});
+
   lv_obj_t *rect = lv_obj_create(lv_screen_active());
   lv_obj_set_pos(rect, x, y);
   lv_obj_set_size(rect, width, height);
 }
 
-void Creator::triangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+void Creator::triangle(string triangle_Name, int x1, int y1, int x2, int y2,
+                       int x3, int y3) {
+  // Check for duplicate triangle name
+  auto exists = std::find(shape_Triangle_Names.begin(),
+                          shape_Triangle_Names.end(), triangle_Name);
+  if (exists != shape_Triangle_Names.end())
+    return;
+
+  shape_Triangle_Names.push_back(triangle_Name);
+  triangles.push_back({triangle_Name, x1, y1, x2, y2, x3, y3});
+
   const lv_point_precise_t line1_points[2] = {{x1, y1}, {x2, y2}};
   const lv_point_precise_t line2_points[2] = {{x2, y2}, {x3, y3}};
   const lv_point_precise_t line3_points[2] = {{x3, y3}, {x1, y1}};
