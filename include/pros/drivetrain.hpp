ae0de1313730abf58043368d3798e70b7d4db0c5
Hepodon <brandonlewis0120@gmail.com>
2025-05-02 20:47:06 -0500

    It works now

--- include/pros/drivetrain.hpp
+++ include/pros/drivetrain.hpp
@@ -1,14 +1,35 @@
-#pragma once
 #include "pros/motors.hpp"
+#include <cstdint>
 
 class DrivetrainDualM {
-public:
-  DrivetrainDualM(pros::Motor left, pros::Motor right);
-
-  void drive_forward(int speed);
-  void stop();
-
 private:
   pros::Motor _left;
   pros::Motor _right;
+  int _gearratio;
+  int _wheelbasewidth;
+  int _wheeldiameter;
+
+public:
+  DrivetrainDualM(pros::Motor left, pros::Motor right, float gearratio,
+                  uint8_t wheelbasewidth, uint8_t wheeldiameter)
+      : _left(left), _right(right), _gearratio(gearratio),
+        _wheelbasewidth(wheelbasewidth), _wheeldiameter(wheeldiameter) {}
+
+  void drive(int8_t speed) {
+    _left.move(speed);
+    _right.move(speed);
+  }
+
+  void stop() {
+    _left.move(0);
+    _right.move(0);
+  }
+
+  void turn(bool right, uint8_t turnDegrees, int8_t velocity) {
+    int direction = right ? 1 : -1;
+    int motorDegrees = (_wheelbasewidth * turnDegrees) / _wheeldiameter;
+
+    _left.move_relative(direction * motorDegrees, velocity);
+    _right.move_relative(-direction * motorDegrees, velocity);
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@
 #include "pros/motor_group.hpp"
 #include "pros/motors.h"
 
-DrivetrainDualM drive(pros::Motor(1), pros::Motor(9));
+using namespace pros;
+
+DrivetrainDualM drivetrain(Motor(1), Motor(20), 1, 10, 4);
 
 void initialize() { lvgl_init(); }
 
@@ -14,4 +16,8 @@ void competition_initialize() {}
 
 void autonomous() {}
 
-void opcontrol() {}
+void opcontrol() {
+  drivetrain.drive(12);
+  drivetrain.turn(true, 90, 12);
+  drivetrain.stop();
+}
